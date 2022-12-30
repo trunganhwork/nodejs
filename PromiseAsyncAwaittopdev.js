28a02e83252154d6eee2668e4fc14f35f84b12c2
phamtrunganhbi <phamtrunganhbi@gmail.com>
2022-12-30 18:34:46 +0700

    add more notes

--- PromiseAsyncAwaittopdev.js
+++ PromiseAsyncAwaittopdev.js
@@ -39,3 +39,41 @@ promise()
 		console.log(result1);
 		return anotherPromise()
 	})
+	.then(result2 => {console.log(result2)})
+	.catch(error => {console.error(error)});
+
+const p = Promise.resolve(12)
+	.then(result1 => {console.log(result1)})
+	.then(result2 => Promise.reject(new Error('dung lai nhanh')))
+	.then(() => '')
+	.catch(err => console.error(err));
+
+
+async function(){
+	try{
+		const user = await api.getUSer('pikalong');
+		const post = await api.getPostofUser(user);
+		const comments = await api.getCommentsOfPosts(post)
+	} catch(err) {
+		console.error(err);
+	}
+}
+
+Promise.resolve(1)
+	.then(2)
+	.then(console.log);
+console.log('This prints 1');
+
+Promise.resolve(1)
+	.then(Promise.resolve(2))
+	.then(console.log);
+console.log('This also prints 1');
+
+Promise.resolve(1)
+	.then(() => 2)
+	.then(console.log);
+Promise.resoolve(1)
+	.then(() => Promise.resolve(2))
+	.then(console.log);
+console.log('This prints 2')
+

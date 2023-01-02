091f252e65fbb0dc0e2b0873695a622fef5d4432
phamtrunganhbi <phamtrunganhbi@gmail.com>
2023-01-02 22:38:35 +0700

    020123 test more and asyncawait

--- JestTest.test.js
+++ JestTest.test.js
@@ -97,3 +97,10 @@ test('the test fail with an error', async () => {
 		expect(e).toMatch('error');
 	}
 });
+
+test('the data is peanut butter', async () => {
+	await expect(fetchData()).resolves.toBe('peanut butter');
+});
+test('the data is peanut butter', async () => {
+	await expect(fetchData()).rejects.toMatch('error');
+})
